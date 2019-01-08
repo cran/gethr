77b009b8fcc380dd20cbe5aa5c442a97eb17680b
Vicente Garcia Diaz <garciavicente@uniovi.es>
2019-01-08 17:00:20 +0000

    version 0.1.0

---
+++ R/gethr.R
@@ -0,0 +1,25 @@
+#' gethr: Access to Ethereum-based Blockchains through Geth Nodes Using the R
+#' Language
+#'
+#' The gethr package provides full access to the Geth command line interface for
+#' running full Ethereum nodes. With gethr it is possible to carry out different
+#' tasks such as mine ether, transfer funds, create contacts, explore block
+#' history, etc. The package also provides access to all the available APIs. The
+#' officially exposed by Ethereum blockchains (\code{eth}, \code{shh},
+#' \code{web3}, \code{net}) and some provided directly by Geth (\code{admin},
+#' \code{debug}, \code{miner}, \code{personal}, \code{txpool}).
+#'
+#' @family admin functions
+#' @family debug functions
+#' @family eth functions
+#' @family ether functions
+#' @family miner functions
+#' @family net functions
+#' @family personal functions
+#' @family shh functions
+#' @family txpool functions
+#' @family web3 functions
+#'
+#' @docType package
+#' @name gethr
+NULL
---
+++ README.md
@@ -0,0 +1,20 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+gethr
+=====
+
+[![Build Status](https://travis-ci.org/vicegd/gethr.svg?branch=master)](https://travis-ci.org/vicegd/gethr)
+
+Overview
+--------
+
+The goal of gethr is to provide access to Ethereum-based blockchains through Geth nodes using the R language.
+
+Installation
+------------
+
+You can install the development version from [GitHub](https://github.com/vicegd/gethr) with:
+
+``` r
+devtools::install_github("vicegd/gethr")
+```
